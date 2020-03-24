b3503a3c274adb581d05249d8b6633cd2ef4e52c
mikisaka23 <mikisaka23@gmail.com>
2020-03-24 11:01:51 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "instanceid" {
   description = "Instance ID"
-  value       = moudule.aws_instance.scalr.id
+  value       = aws_instance.scalr.id
 }
 
 output "publicip" {
   description = "Public IP"
-  value       = moudule.aws_instance.scalr.public_ip
+  value       = aws_instance.scalr.public_ip
 }
 
 output "privateip" {
   description = "Private IP"
-  value       = moudule.aws_instance.scalr.private_ip
+  value       = aws_instance.scalr.private_ip
 }

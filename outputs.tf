116382224c44a482adf476e486c0d2fb342b2ab0
mikisaka23 <mikisaka23@gmail.com>
2020-03-24 11:12:53 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "instanceid" {
   description = "Instance ID"
-  value       = aws_instance.scalr.id
+  value       = scalr.id
 }
 
 output "publicip" {
   description = "Public IP"
-  value       = aws_instance.scalr.public_ip
+  value       = scalr.public_ip
 }
 
 output "privateip" {
   description = "Private IP"
-  value       = aws_instance.scalr.private_ip
+  value       = scalr.private_ip
 }
